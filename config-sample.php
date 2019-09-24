f466651b7139e436a7eef4398eeb0f17d4749d75
janforman <info@janforman.com>
2019-09-24 11:08:55 +0200

    config update

--- config-sample.php
+++ config-sample.php
@@ -1,3 +1,11 @@
 <?php
 $public_modules = array('account');
 $enabled_modules = array('');
+$mariadb['ip'] = 'localhost';
+$mariadb['db'] = 'framework';
+$mariadb['name'] = 'root';
+$mariadb['pass'] = '';
+define('sitename', '');
+define('domain', 'janforman.com');
+define('salt', 'tu@S+N^Q4r-4}(w/');
+define('version', '1.00');
