305931114df893e4c3dac32cb21c85e0b13eed6c
Jan Forman <48495095+janforman@users.noreply.github.com>
2019-09-24 20:31:33 +0200

    Remove lines

--- legacy.php
+++ legacy.php
@@ -1,30 +1,26 @@
 <?php
 function sql_query($query)
 {
-    $res = @mysqli_query($GLOBALS['mysqli'], $query);
-    
+    $res = @mysqli_query($GLOBALS['mysqli'], $query);    
     return $res;
 }
 
 function sql_num_rows($res)
 {
-    $rows = mysqli_num_rows($res);
-    
+    $rows = mysqli_num_rows($res);    
     return $rows;
 }
 
 function sql_fetch_row($res)
 {
-    $row = mysqli_fetch_row($res);
-    
+    $row = mysqli_fetch_row($res);    
     return $row;
 }
 
 function sql_fetch_array($res)
 {
     $row = array();
-    $row = mysqli_fetch_array($res, MYSQLI_BOTH);
-    
+    $row = mysqli_fetch_array($res, MYSQLI_BOTH);   
     return $row;
 }
 
