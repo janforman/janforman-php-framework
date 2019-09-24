dcbfb8e4aad2251800906a149705520528bb79d1
janforman <info@janforman.com>
2019-09-24 11:13:15 +0200

    init update

--- init.php
+++ init.php
@@ -1,2 +1,7 @@
 <?php
 require './config.php';
+mb_internal_encoding('UTF-8');
+$GLOBALS['mysqli'] = mysqli_connect($mariadb['ip'], $mariadb['name'], $mariadb['pass'], $mariadb['db']);
+if (!$GLOBALS['mysqli']) {
+    log_error('mariadb disconnected');
+}
