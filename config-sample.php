e8202f38947d0e6f163489d274a681dffc626d77
Jan Forman <48495095+janforman@users.noreply.github.com>
2019-09-26 14:29:04 +0200

    add ldap config

--- config-sample.php
+++ config-sample.php
@@ -10,3 +10,8 @@ define('webmaster', 'noreply@janforman.com');
 define('domain', 'https://janforman.com');
 define('salt', 'tu@S+N^Q4r-4}(w/');
 define('version', '1.00');
+$GLOBALS['ldap_host'] = "10.0.0.1";
+$GLOBALS['ldap_dn'] = "ou=CORP,dc=corp,dc=local";
+$GLOBALS['ldap_user_group'] = "";
+$GLOBALS['ldap_manager_group'] = "";
+$GLOBALS['ldap_usr_dom'] = '@corp.local';
