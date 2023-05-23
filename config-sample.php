5a8cdc66e6424214e65beb19a664a48b74548f27
Jan Forman <48495095+janforman@users.noreply.github.com>
2023-05-23 08:31:03 +0200

    Add description to config

--- config-sample.php
+++ config-sample.php
@@ -10,6 +10,7 @@ define('webmaster', 'noreply@janforman.com');
 define('domain', 'https://janforman.com');
 define('salt', 'tu@S+N^Q4r-4}(w/');
 define('version', '1.00');
+define('description', 'Sample website description');
 $GLOBALS['ldap_host'] = "10.0.0.1";
 $GLOBALS['ldap_dn'] = "ou=CORP,dc=corp,dc=local";
 $GLOBALS['ldap_user_group'] = "";
